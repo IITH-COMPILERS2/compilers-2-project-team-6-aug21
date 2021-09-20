13e53c96238ee4dbf38d513310aa7ed50486e834
sathwik22 <74591981+sathwik22@users.noreply.github.com>
2021-09-20 16:03:21 +0530

    Delete main.ml

--- main.ml
+++
@@ -1,9 +0,0 @@
-open Printf
-let main () =
-  try
-    let lexbuf = Lexing.from_channel stdin in
-    (* while true do *)
-      Parser.datatype Lexer.tokens lexbuf
-    (* done *)
-  with End_of_file -> printf "plp"; exit 0
-let _ = Printexc.print main ()
